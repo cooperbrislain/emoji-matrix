671168dd6de7affb155e79f8ec22174633d57c61
Cooper Brislain <cbrislain@gmail.com>
2018-06-29 16:05:56 -0700

    nitial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var emojione = require('emojione');
+var child_process = require('child_process');
+var LedMatrix = require("node-rpi-rgb-led-matrix");
+var mqtt = require("mqtt");
+var fs = require('fs');
+
+var matrix = new LedMatrix(32);
+matrix.fill(0,0,0);
+
+var pubsub_client  = mqtt.connect('mqtt://localhost')
+var app = express();
+var proc;
+var filename;
+
+app.use(express.static('/custom-png'));
+app.use(express.static('/emojione-png'));
+
+app.get('/emoji/:arg', function (req, res) {
+    matrix.fill(0,0,0);
+    if (fs.existsSync('custom-png/' + req.params.arg + '.png')) {
+        console.log(filename = 'custom-png/' + req.params.arg + '.png');
+    } else if(str = emojione.toImage(':' + req.params.arg + ': ')) {
+        console.log(filename = 'emojione-png/' + str.match(/([^/]+.png)/gius)[1]);
+    }
+    child_process.exec('sudo killall led-image-viewer', function() {
+        child_process.exec('sudo led-image-viewer -C --led-pixel-mapper "U-mapper;Rotate:270" ' + filename);
+    });
+    res.send('<image src="/' + filename + '"></image>');
+});
+
+pubsub_client.on('connect', () => {
+    pubsub_client.subscribe('/announce');
+    pubsub_client.subscribe('/led-matrix/emoji');
+    pubsub_client.publish('/announce', 'How are you gentlemen!');
+});
+
+pubsub_client.on('message', function (topic, message) {
+    console.log('[' + topic + ']' + message);
+    if (topic === '/led-matrix/emoji') {
+        console.log('Looking for custom-png/' + message + '.png...');
+        if (fs.existsSync('custom-png/' + message + '.png')) {
+            console.log(filename = 'custom-png/' + message + '.png');
+        } else if(str = emojione.toImage(':' + message + ': ')) {
+            console.log(filename = 'emojione-png/' + str.match(/([^/]+.png)/gius)[1]);
+        }
+        child_process.exec('sudo killall led-image-viewer', function() {
+            child_process.exec('sudo led-image-viewer -C --led-pixel-mapper "U-mapper;Rotate:270" ' + filename);
+        });
+    }
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000!');
+});
