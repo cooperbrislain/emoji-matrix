624958116645cb5369813373e7197516f687a913
Cooper Brislain <cbrislain@gmail.com>
2018-06-29 20:07:37 -0700

    stuff

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ app.get('/emoji/:arg', function (req, res) {
 app.get('/gif/:arg', function (req, res) {
     matrix.fill(0,0,0);
     if (fs.existsSync('animated-gif/' + req.parameters.arg + '.gif')) {
-        console.log(filename = 'custom-gif');
+        console.log(filename = 'animated-gif/' + req.parameters.arg + '.gif');
+        child_process.exec('sudo killall led-image-viewer', function() {
+            child_process.exec('sudo led-image-viewer -C --led-pixel-mapper "U-mapper;Rotate:270" ' + filename);
+        });
     }
 });
 
